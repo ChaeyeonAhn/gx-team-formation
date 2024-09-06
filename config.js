b58a7538e19125042f5ba777b9f23fb40ebb5af4
Donghyeok Ma <61783385+mdngmdng@users.noreply.github.com>
2024-09-06 15:15:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    origin: 'http://192.168.50.147:3000',
+    origin: 'http://34.64.85.65:3000',
     port: 5002, // Express 서버 포트
     wsPort: 751, // WebSocket 서버 포트
     mongoURL: 'mongodb+srv://admin:0423504564@gx-mongo.eau3o.mongodb.net',
