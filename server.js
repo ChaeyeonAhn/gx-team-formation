ce0fcac9442f093b6cdf879c3760ac4370646015
chacha <chacha@sparcs.org>
2025-02-27 15:04:58 +0900

    feat: add console.log

--- server.js
+++ server.js
@@ -245,6 +245,8 @@ app.post("/update-data", async (req, res) => {
 
       for (key in keysWithDifferentValue) {
         const wsClient = clients.get(key);
+        console.log(key);
+        console.log(wsClient);
         if (wsClient && wsClient.readyState === WebSocket.OPEN) {
           wsClient.send(
             JSON.stringify({
